c149782692067905d08d488ec1d9b4b47699d74e
17echo8-maker <17echo8@gmail.com>
2026-09-10 21:48:50 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,10 @@
-const CACHE='nce3-v1';
+const CACHE='nce3-v2';
 const ASSETS=['./','./index.html','./lesson42.json','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
-self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+self.addEventListener('activate',e=>e.waitUntil(
+  caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))
+));
+self.addEventListener('fetch',e=>e.respondWith(
+  fetch(e.request).then(r=>{const c=r.clone();caches.open(CACHE).then(cache=>cache.put(e.request,c));return r})
+  .catch(()=>caches.match(e.request))
+));
