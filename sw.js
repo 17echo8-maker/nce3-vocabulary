f7361fe737981a16baf875dc4fbdb32e68af5b56
17echo8-maker <17echo8@gmail.com>
2026-09-11 22:58:37 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='nce3-v10';
+const CACHE='nce3-v11';
 const ASSETS=['./','./index.html','./lessons.json','./lesson42.json','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
 
 self.addEventListener('install',e=>{
